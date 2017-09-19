57a76f1806837d1183a19ba32fa7779430cd1ff7
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-09-19 10:49:59 -0400

    second hand moves with each second

--- app.js
+++ app.js
@@ -3,6 +3,15 @@
 (function(){
 
     'use strict';
-    
 
+    let secondHand = document.querySelector('.second-hand');
+    
+    function setDate() {
+        let now = new Date();
+        let seconds = now.getSeconds();
+        let secondsDegrees =((seconds / 60) * 360) + 90;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;        
+    }
+
+    setInterval(setDate, 1000);
 }());
